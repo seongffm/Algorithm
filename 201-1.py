03f65086e4e32ba77d54e65217cd157bafc4673c
김성훈 <167969787+seongffm@users.noreply.github.com>
2024-10-02 20:17:23 +0900

    17413

--- 201-1.py
+++ 201-1.py
@@ -1,8 +1,25 @@
+#17413
 import sys
 input = sys.stdin.readline
 
-for _ in range(int(input())):
-    a=list(map(str,input().strip().split()))
-    for i in a:
-        print(i[::-1],end=' ')
-    print()
+s=input().strip()+" "
+st=[]
+res=""
+check=True
+for i in s:
+    if i=="<":
+        check=False
+        for _ in range(len(st)):
+            res+=st.pop()
+    st.append(i)
+    if i==">":
+        check=True
+        for _ in range(len(st)):
+            res+=st.pop(0)
+    elif i==" "and check:
+        st.pop()
+        for _ in range(len(st)):
+            res+=st.pop()
+        res+=" "
+print(res.strip())
+
